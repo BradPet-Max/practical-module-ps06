86287cf5c5d19d2fbe1bbc25112a4de6e77add8b
Peter Mavazhe <peterbmavavazhe@gmail.com>
2026-07-13 13:05:01 +0200

    Add arithmetic, string, object, and array expression examples

---
+++ PA0603_create_expressions.py
@@ -0,0 +1,26 @@
+# Question 1 – Arithmetic
+num1 = 10
+num2 = 5
+result = num1 * num2
+print(result)
+
+
+# Question 2 – String Expression
+city = "Polokwane"
+print("Welcome to " + city)
+
+
+#Question 3 – Object Expression
+student = {
+    "name": "Lerato",
+    "marks": 75
+    }
+
+print(student["name"] + " scored " + str(student["marks"]) + " marks")
+
+
+#Question 5 – Array Expression
+numbers = [10, 20, 30]
+total = sum(numbers)
+
+print(total)
