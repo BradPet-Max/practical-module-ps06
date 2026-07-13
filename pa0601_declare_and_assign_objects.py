8f50aaed8cbadbdf7caa95766a1b38d2ba46eea6
Peter Mavazhe <peterbmavavazhe@gmail.com>
2026-07-13 12:41:29 +0200

    first commit

---
+++ pa0601_declare_and_assign_objects.py
@@ -0,0 +1,28 @@
+#Create an object called student with the required attributes
+class Student:
+    pass
+
+student = Student()
+student.name = "Peter"
+student.studentNumber = "ST12345"
+student.course = "Data Science"
+student.yearLevel = 1
+
+print(student.name)
+print(student.studentNumber)
+print(student.course)
+print(student.yearLevel)
+
+#Create an empty object called vehicle, then assign
+class Vehicle:
+    pass
+
+vehicle = Vehicle()
+
+vehicle.brand = "Toyota"
+vehicle.colour = "White"
+vehicle.registrationNumber = "CA123456"
+
+print(vehicle.brand)
+print(vehicle.colour)
+print(vehicle.registrationNumber)
